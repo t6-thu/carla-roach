66ee7ed7ffb6d51ac2426bad493932290812fd0d
t6-thu <t6.da.thu@gmail.com>
2023-08-24 02:03:54 -0700

    get actions

--- model_loader.py
+++ model_loader.py
@@ -1,9 +1,14 @@
 import wandb
-from omegaconf import OmegaConf
 from agents.rl_birdview.rl_birdview_agent import RlBirdviewAgent
 import ipdb
-import os
-
+import gym
+import json
+from pathlib import Path
+import hydra
+from omegaconf import DictConfig, OmegaConf
+import logging
+import os.path
+import sys
 class ModelLoader:
     def __init__(self, path_to_conf_file='config_agent.yaml'):
         self.path_to_conf_file = path_to_conf_file
@@ -16,15 +21,27 @@ class ModelLoader:
     def load_wandb_model(self):
         # Load the model using your agent class
         self.agent = RlBirdviewAgent(path_to_conf_file='config_agent.yaml')
+    
+    # def run_environment(self, env_setup, max_step=None):
+    #     # Create the environment
+    #     env = gym.make(env_setup['env_id'], obs_configs=obs_configs, reward_configs=reward_configs,
+    #                    terminal_configs=terminal_configs, host=cfg.host, port=cfg.port,
+    #                    seed=cfg.seed, no_rendering=cfg.no_rendering, **env_setup['env_configs'])
 
-# Create an instance of ModelLoader to load the model
-model_loader = ModelLoader()
+    #     # Run the environment using the loaded agent
+    #     list_render, ep_stat_dict, ep_event_dict, timestamp = run_single(
+    #         run_name, env, {'actor_id': self.agent}, agents_log_dir, False, max_step)
 
+    #     # Close the environment
+    #     env.close()
 
-# # Assuming you have a model_loader instance
-# loaded_agent = model_loader.agent
+    #     return list_render, ep_stat_dict, ep_event_dict, timestamp
 
-# # Use the loaded agent as needed
-# observation = ...  # Your input observation
-# action = loaded_agent.predict(observation)
+    def get_action(self, obs, timestamp):
+        # # Create an instance of ModelLoader to load the model
+        # model_loader = ModelLoader()
+        # Call the run_step function using the loaded agent
+        control = self.agent.run_step(obs, timestamp)
+        
+        return control
 
